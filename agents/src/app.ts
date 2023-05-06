a83e8dce8ebd8fde1ecb0878a6135af136ce6541
Christian Simms <csimms@botonomy.com>
2023-05-05 20:17:29 -0400

    Reorg.

--- agents/src/app.ts
+++ agents/src/app.ts
@@ -2,10 +2,25 @@ import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
+class Arena extends Scene {
+    constructor(engine: Engine, public app: App) {
+        super(engine);
+
+        const scene = this;
+        const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(app.canvas, true);
+
+        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    }
+}
+
 class App {
+    canvas: HTMLCanvasElement;
     constructor() {
         // create the canvas html element and attach it to the webpage
         const canvas = document.createElement("canvas");
+        this.canvas = canvas;
         canvas.style.width = "100%";
         canvas.style.height = "100%";
         canvas.id = "gameCanvas";
@@ -13,12 +28,7 @@ class App {
 
         // initialize babylon scene and engine
         const engine = new Engine(canvas, true);
-        const scene = new Scene(engine);
-
-        const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        const scene = new Arena(engine, this);
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
